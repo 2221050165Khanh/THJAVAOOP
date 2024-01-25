59249e74fdd5eb7ed2e45db85158528b4ad95b49
2221050165Khanh <nxuaankhanh204@gmail.com>
2024-01-25 17:18:55 +0700

    JavaOOP

---
+++ LAB2/src/Slide93Chuong2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Slide93Chuong2{
+    public static void main(String[] args){
+        String Name;
+        int Year;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Nhap Ten:"); Name=sc.nextLine();
+        System.out.print("Nhap nam sinh:"); Year=sc.nextInt();
+        sc.close();
+        if(Year-new Date >16)
+        
+    }
+}
